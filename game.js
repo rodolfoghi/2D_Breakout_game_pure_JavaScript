b568e1cfc1245315ce7a8ed0523edc247d918d8b
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2019-02-27 17:54:04 -0300

    Criando o paddle

--- game.js
+++ game.js
@@ -6,6 +6,18 @@ let dx = 2;
 let dy = -2;
 let ballRadius = 10;
 
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) /  2;
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -17,7 +29,8 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-
+    drawPaddle();
+    
     if (x + dx > canvas.width || x + dx < ballRadius) {
         dx = -dx;
     }
