4b2aa12714008f2ac6aee00689a94259b01fd973
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2019-02-27 18:05:37 -0300

    Implementando o game over

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ let ballRadius = 10;
 
 let paddleHeight = 10;
 let paddleWidth = 75;
-let paddleX = (canvas.width - paddleWidth) /  2;
+let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
@@ -18,8 +18,7 @@ document.addEventListener("keyup", keyUpHandler, false);
 function keyDownHandler(e) {
     if (e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = true;
-    }
-    else if (e.key == "Left" || e.key == "ArrowLeft") {
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = true;
     }
 }
@@ -27,8 +26,7 @@ function keyDownHandler(e) {
 function keyUpHandler(e) {
     if (e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = false;
-    }
-    else if (e.key == "Left" || e.key == "ArrowLeft") {
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
     }
 }
@@ -58,14 +56,21 @@ function draw() {
         dx = -dx;
     }
 
-    if (y + dy > canvas.height || y + dy < ballRadius) {
+    if (y + dy < ballRadius) {
         dy = -dy;
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval); // Needed for Chrome to end game
+        }
     }
 
     if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 7;
-    }
-    else if (leftPressed && paddleX > 0) {
+    } else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
     }
 
@@ -73,4 +78,4 @@ function draw() {
     y += dy;
 }
 
-setInterval(draw, 10);
+let interval = setInterval(draw, 10);
